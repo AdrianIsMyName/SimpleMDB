0ce376a10c2a1d7b91cbab8bb5f2a5082381e251
Adrian <adriands778@gmail.com>
2026-04-25 17:08:12 -0400

    Gonna be honest, I struggled for a bit and I'm not 100% sure what I added and what I changed.  I tested to make sure everything works though.  Still not my proudest 'commit'.

--- src/Smdb.Ssr/App.cs
+++ src/Smdb.Ssr/App.cs
@@ -1,6 +1,9 @@
 using System.Net;
-using System.Collections;
 using Shared.Http;
+
+using Smdb.Core.Db;
+using Smdb.Core.Users;
+using Smdb.Api.Users;
 
 namespace SimpleMDB;
 
@@ -11,7 +14,7 @@ public class App
 
 	public App()
 	{
-		string host = "http://localhost:8080/";
+		string host = "http://localhost:8081/";
 		server = new HttpListener();
 		router = new HttpRouter();
 
@@ -19,10 +22,27 @@ public class App
 
 		Console.WriteLine($"Server listening on... " + host);
 
-		var authController = new AuthController();
+		var db = new MemoryDatabase();
 
+		var userRepo = new MemoryUserRepository(db);
+		var userServ = new DefaultUserService(userRepo);
+		var userCtrl = new UsersController(userServ);
+		var authController = new AuthController(userServ);
+
+		// Add middleware
+		router.Use(HttpUtils.StructuredLogging);
+		router.Use(HttpUtils.CentralizedErrorHandling);
+		router.Use(HttpUtils.AddResponseCorsHeaders);
+		router.Use(HttpUtils.DefaultResponse);
+		router.Use(HttpUtils.ParseRequestUrl);
+		router.Use(HttpUtils.ParseRequestQueryString);
+
+		// Enable route matching
 		router.UseSimpleRouteMatching();
+
+		// Define routes
 		router.MapGet("/", authController.LandingPageGet);
+		router.MapGet("/users", userCtrl.ReadUsers);
 	}
 
 	public async Task Start()
--- src/Smdb.Ssr/auth/AuthController.cs
+++ src/Smdb.Ssr/auth/AuthController.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Net;
 using Shared.Http;
+using Smdb.Core.Users;
 
 namespace SimpleMDB;
 
 public class AuthController
 {
-	public AuthController()
+	private readonly IUserService userService;
+
+	public AuthController(IUserService userService)
 	{
-		
+		this.userService = userService;
 	}
 
 	public async Task LandingPageGet(HttpListenerRequest req, HttpListenerResponse res, Hashtable options, Func<Task> next)
 	{
-    string html = HtmlTemplates.Base("SimpleMDB", "Landing Page", "Hello World!");
-    await HttpUtils.SendOkResponse(req, res, options, html, "text/html");
+		string html = HtmlTemplates.Base("SimpleMDB", "Landing Page", "Hello World!");
+		await HttpUtils.SendOkResponse(req, res, options, html, "text/html");
 	}
 }
